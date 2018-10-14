38396f3cdf8a34e3bc8ba3e6c3e48c52587931e9
Chris Joy <chrisjoydev@gmail.com>
2018-10-15 10:21:09 +1100

    Stub recommendations method

--- src/api/recommendation/controller.js
+++ src/api/recommendation/controller.js
@@ -1,21 +1,25 @@
 const _ = require('lodash');
 const { success, notFound } = require('../../services/response/');
 const Interaction = require('../interaction/model');
+const Movie = require('../movie/model');
 
 exports.index = async ({ querymen: { query, select, cursor }, user }, res, next) => {
 	// Grab all watched and liked movies from user
-	const myMovies = Interaction
-		.find({
-			user: user.id,
-			hasLiked: true,
-			hasWatched: true,
-		})
-		.populate('movie')
-		.map(interaction => interaction.movie);
+	// const myMovies = Interaction
+	// 	.find({
+	// 		user: user.id,
+	// 		hasLiked: true,
+	// 		hasWatched: true,
+	// 	})
+	// 	.populate('movie')
+	// 	.map(interaction => interaction.movie);
 	// Grab users that've watched similar movies
-
-	const
+	const movies = await Movie.find().limit(20);
 	return res.json({
-		user: user.id,
+		movies,
 	});
+	// const
+	// return res.json({
+	// 	user: user.id,
+	// });
 };
