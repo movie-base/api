c1ee80d5e692f7fdc8ed5ec6d7667fd240fbbb01
Chris Joy <chrisjoydev@gmail.com>
2018-10-19 00:35:49 +1100

    Added recommendations route

--- src/api/recommendation/controller.js
+++ src/api/recommendation/controller.js
@@ -1,52 +1,53 @@
 const _ = require('lodash');
 const Interaction = require('../interaction/model');
-const Movie = require('../movie/model');
 
-exports.index = async ({ user }, res, next) => {
-	// // 1. Grab all watched and liked movies from target user's
-	// // interactions
-	// // output: array of movieIds watched by the target user
-	// const userLikedInteractions = await Interaction
-	// 	.find({
-	// 		user: user.id,
-	// 		hasLiked: true,
-	// 		hasWatched: true,
-	// 	});
-	// const userLikedMovies = _.uniq(userLikedInteractions, 'movie')
-	// 	.map(interaction => interaction.movie);
-	// // 2. Grab users that've watched similar movies
-	// // output: array of users with similar watched and liked movies
-	// const similarUserInteractions = await Interaction
-	// 	.find({
-	// 		movie: { $in: userLikedMovies },
-	// 		hasLiked: true,
-	// 		hasWatched: true,
-	// 	});
-	// return res.json({
-	// 	similarUserInteractions,
-	// });
+// Max number of similar users comparisons.
+const MAX_USER_COMPARISONS = 30;
+const MAX_RECOMMENDATIONS = 100;
 
-
-	// // 3. Sort similar users by frequency
-	// // output: sorted array of userids
-	// const similarUsersfreq = similarUsers.reduce((obj, val) => {
-	// 	obj[val] = (obj[val] || 0) + 1;
-	// 	return obj;
-	// }, {});
-	// const similarUsersSorted = Object.keys(similarUsersfreq)
-	// 	.sort((a, b) => similarUsersfreq[b] - similarUsersfreq[a]);
-	// // 4. Grab all movies watched and liked by similar users, but not watched by
-	// // the current user
-	// const similarUserMovies = await Interaction.find({
-	// 	user: {
-	// 		$in: similarUsersSorted.map(id => ObjectId(id)),
-	// 	},
-	// 	hasLiked: true,
-	// 	hasWatched: true,
-	// }).map(interaction => interaction.movie);
-
-	const movies = await Movie.find().limit(20);
-	return res.json({
-		movies,
-	});
+exports.index = async ({ user, querymen: { select, cursor } }, res, next) => {
+	// 1. Grab all liked movies by target user
+	const userLikedInteractions = await Interaction
+		.find({
+			user: user.id,
+			hasLiked: true,
+			hasWatched: true,
+		});
+	const userLikedMovies = _.uniq(userLikedInteractions, 'movie')
+		.map(interaction => interaction.movie);
+	// 2. Grab users that've watched similar movies
+	const similarUserInteractions = await Interaction
+		.find({
+			user: { $ne: user.id },
+			movie: { $in: userLikedMovies },
+			hasLiked: true,
+			hasWatched: true,
+		});
+	// 3. Sort similar users by frequency
+	const similarUsers = _.chain(similarUserInteractions)
+		.countBy('user')
+		.toPairs()
+		.sortBy(1)
+		.reverse()
+		.map(0)
+		.splice(0, MAX_USER_COMPARISONS)
+		.value();
+	// 4. Grab all movies liked by similar users, but not watched by the target user
+	const intersectionInteractions = await Interaction.find({
+		user: {
+			$in: similarUsers,
+		},
+		movie: {
+			$nin: userLikedMovies,
+		},
+		hasLiked: true,
+		hasWatched: true,
+	}, select, cursor)
+		.populate('movie');
+	// 5. Based on the intersected movies (ie. movies liked by similar users -
+	// and not watched by target user) create a recommendations list
+	const movies = _.chain(intersectionInteractions.map(i => i.movie))
+		.uniq('id')
+		.value();
+	return res.json({ movies });
 };
