6a944032b7a30ca9dd2395c631edcfead3e1ae27
Chris Joy <chrisjoydev@gmail.com>
2018-10-21 21:50:17 +1100

    Exclude want to watch from recommendations

--- src/api/recommendation/controller.js
+++ src/api/recommendation/controller.js
@@ -17,7 +17,7 @@ exports.index = async ({ user, querymen: { select, cursor } }, res) => {
 			hasLiked: true,
 			hasWatched: true,
 		});
-	const userLikedMovies = _.uniq(userLikedInteractions, 'movie')
+	const userLikedMovieIds = _.uniq(userLikedInteractions, 'movie')
 		.map(interaction => interaction.movie);
 	const userDisikedInteractions = await Interaction
 		.find({
@@ -25,30 +25,33 @@ exports.index = async ({ user, querymen: { select, cursor } }, res) => {
 			hasLiked: false,
 			hasWatched: true,
 		});
-	const userDislikedMovies = _.uniq(userDisikedInteractions, 'movie')
+	const userDislikedMovieIds = _.uniq(userDisikedInteractions, 'movie')
 		.map(interaction => interaction.movie);
-	const userWantsToWatchInteractions = await Interaction
+	const userWantToWatchInteractions = await Interaction
 		.find({
 			user: user.id,
-			wantsToWatch: true,
+			wantToWatch: true,
 		});
-	const userWantsToWatchMovies = _.uniq(userWantsToWatchInteractions, 'movie')
+	const userWantToWatchMovieIds = _.uniq(userWantToWatchInteractions, 'movie')
 		.map(interaction => interaction.movie);
-	const userMovies = _.uniq(
-		_.flatten([userLikedMovies, userDislikedMovies, userWantsToWatchMovies]),
-		_.isEqual,
-	);
+	const excludedMovieIds = [
+		...userLikedMovieIds,
+		...userDislikedMovieIds,
+		...userWantToWatchMovieIds,
+	];
 	// 2. Grab users that've watched similar movies
 	const similarUserInteractions = await Interaction
 		.find({
 			$and: [
+				{ user: { $ne: user.id } },
+				{ movie: { $in: userLikedMovieIds } },
+				{ movie: { $nin: userDislikedMovieIds } },
 				{
-					user: { $ne: user.id },
-					movie: { $in: userLikedMovies },
-					hasLiked: true,
-					hasWatched: true,
+					$or: [
+						{ hasLiked: true, hasWatched: true },
+						{ wantToWatch: true, hasWatched: false },
+					],
 				},
-				{ movie: { $nin: userDislikedMovies } },
 			],
 		});
 	// 3. Sort similar users by frequency
@@ -62,18 +65,18 @@ exports.index = async ({ user, querymen: { select, cursor } }, res) => {
 		.value();
 	// 4. Grab all movies liked by similar users, but not watched by the target user
 	const intersectionInteractions = await Interaction.find({
-		user: { $in: similarUsers },
-		movie: { $nin: userMovies },
-		hasLiked: true,
-		hasWatched: true,
+		$and: [
+			{ user: { $in: similarUsers } },
+			{ movie: { $nin: excludedMovieIds } },
+			{ hasLiked: true, hasWatched: true },
+		],
 	}, select, cursor)
 		.populate('movie');
 	// 5. Based on the intersected movies (ie. movies liked by similar users -
 	// and not watched by target user) create a recommendations list
-	const movies = _.chain(intersectionInteractions.map(i => i.movie))
+	const movies = await _.chain(intersectionInteractions.map(i => i.movie))
 		.uniq('id')
 		.orderBy('year', 'desc')
-		.pull(userMovies)
 		.compact()
 		.value();
 	return res.json({ movies });
